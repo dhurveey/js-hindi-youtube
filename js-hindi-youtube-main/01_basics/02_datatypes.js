80fda8eec1a425410952dbb06e6333aa69eb32fe
नबिन रोकाया <rokayadhurvey@gmail.com>
2024-11-27 11:35:37 +0545

    Add files via upload

---
+++ js-hindi-youtube-main/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert(3+3) // we are using node.js, not browser
+
+
+let name = "nabin"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+//boolean => true or false
+// null => standLOne value
+// undefined => 
+// symbol => unique
+
+//object
+
+ console.log(typeof undefined); //types of undefined
+ console.log(typeof null); //objects
+ 
+ 
