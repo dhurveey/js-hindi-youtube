a5039795caf9cc3b8a762842ca4f7419d0625216
नबिन रोकाया <rokayadhurvey@gmail.com>
2024-12-20 15:29:42 +0000

    new

---
+++ 01_basics/Loops-Pratice.js
@@ -0,0 +1,18 @@
+//Write a for loop to print numbers from 1 to 10.
+
+// Use a while loop to print numbers from 10 to 1.
+
+// Write a do-while loop that prints the numbers from 1 to 5.
+
+// Use a for loop to print all even numbers between 1 and 20.
+
+// Write a for loop to iterate through an array of fruits and log each fruit to the console.
+
+// const fruits = ['apple', 'banana', 'mango', 'orange'];
+
+// Create a multiplication table (1 to 10) using nested for loops
+
+// Write a for loop to print all prime numbers between 1 and 50.
+
+// Write a loop that skips printing numbers divisible by 3 but prints all others between 1 and 30.
+
---
+++ 01_basics/Loops.js
@@ -0,0 +1,78 @@
+//LOOPS
+// for(let i = 1; i <=10; i++) {
+//     console.log("Apna college");
+    
+// }
+//FOR LOOP
+// for( let i =1; i <=1000; i++){
+//     console.log("Nabin rokaya");    
+// }
+
+//While Loop
+
+// console.log("loops has ended");
+
+// let i = 1;
+// while(i<=5) {
+//     console.log("Apna college"
+
+//     );
+//     i++;
+    
+// }
+
+//do-while loop
+
+// i = 20;
+// do{
+//     console.log("Apna college");
+// }while(i <= 5);
+
+//for-of loop
+
+// let str = "JavaScript"
+
+// let size = 0;
+// for(let i of str) {
+//     console.log("i=", i);
+//     size++;
+    
+// }
+
+// console.log(size);
+
+//For in Loop
+
+// let student = {
+//     name : "Nabin rokaya",
+//     age : 18,
+//     cgpa : 3.14,
+//     IsPass : true,
+// };
+
+// for(let i in student){
+//     console.log(i, student);
+    
+// }
+
+//Pratice Q.1
+
+// for(let i = 0; i <= 100; i++ )
+// {
+//     if(i % 2 !== 0)
+//        { console.log("num=", i);
+//       
+
+//Question.1
+
+// for(let i = 1; i <= 10; i++){
+//     console.log("num=", i);
+// }
+
+//Question.2
+
+let i = 1;
+while(i <= 10){
+    console.log("num=", i);
+i--;
+}
