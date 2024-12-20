8cb640ab2f59ca29010a3635d83205b61875fa34
नबिन रोकाया <rokayadhurvey@gmail.com>
2024-12-20 15:46:00 +0000

    new

--- 01_basics/String.js
+++ 01_basics/String.js
@@ -1,3 +1,20 @@
-let str = "Apna College"
+// Access Length
 
-console.log(str[0], str)
+// let str = "Apna College"
+
+// console.log(str[0], str)
+
+//Template Literals
+
+// let specialString = `Hello`;
+// console.log(specialString);
+
+let obj = {
+    item: "pen",
+    price: 10,
+}
+
+// console.log(obj.item);
+
+let input = `the item is ${obj.item} the price is ${obj.price}number`;
+console.log(input);
